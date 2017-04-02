4ebbc97e6692501885ade16d983b06f61a5ad839
Steffen B <stevnash@gmx.de>
2017-04-02 23:57:38 +0200

    Add some Styles

--- src/main/java/de/naju/ahlen/gui/VaadinUI.java
+++ src/main/java/de/naju/ahlen/gui/VaadinUI.java
@@ -6,11 +6,64 @@ import com.vaadin.ui.*;
 import com.vaadin.spring.annotation.SpringUI;
 
 @SpringUI(path = "")
-@Theme("valo")
+@Theme("mytheme")
 public class VaadinUI extends UI {
+
+    private HorizontalSplitPanel wrapper;
+    private VerticalLayout menuWrapper;
+    private VerticalLayout contenHomeWrapper;
 
     @Override
     protected void init(VaadinRequest request) {
-        setContent(new Button("Click me", e -> Notification.show("Hello Spring+Vaadin user!")));
+        wrapper = new HorizontalSplitPanel();
+        wrapper.setSizeFull();
+        wrapper.setSplitPosition(280, Unit.PIXELS);
+        wrapper.setLocked(true);
+
+        menuWrapper = new VerticalLayout();
+        menuWrapper.setWidth(100, Unit.PERCENTAGE);
+        menuWrapper.setMargin(true);
+        menuWrapper.setSpacing(true);
+
+        contenHomeWrapper = new VerticalLayout();
+        contenHomeWrapper.setWidth(100, Unit.PERCENTAGE);
+
+        initMenu();
+
+        Button b1 = new Button("Click me", e -> Notification.show("Hello Spring+Vaadin user!", Notification.Type.TRAY_NOTIFICATION));
+        contenHomeWrapper.addComponent(b1);
+
+        wrapper.addComponent(menuWrapper);
+        wrapper.addComponent(contenHomeWrapper);
+        setContent(wrapper);
+    }
+
+    private void initMenu(){
+        // Button Styling: https://vaadin.com/forum#!/thread/1687729
+        Button bHome = new Button("Home");
+        bHome.setStyleName("menu-button");
+        bHome.addClickListener(event -> {
+            Notification.show("bHome was clicked", Notification.Type.TRAY_NOTIFICATION);
+        });
+
+        Button bFinance = new Button("Einnahmen/Ausgaben");
+        bFinance.setStyleName("menu-button");
+        bFinance.addClickListener(event -> {
+            Notification.show("bFinance was clicked", Notification.Type.TRAY_NOTIFICATION);
+        });
+
+        Button bMembers = new Button("Mitglieder");
+        bMembers.setStyleName("menu-button");
+        bMembers.addClickListener(event -> {
+            Notification.show("bMembers was clicked", Notification.Type.TRAY_NOTIFICATION);
+        });
+
+        Button bSettings = new Button("Einstellungen");
+        bSettings.setStyleName("menu-button");
+        bSettings.addClickListener(event -> {
+            Notification.show("bSettings was clicked", Notification.Type.TRAY_NOTIFICATION);
+        });
+
+        menuWrapper.addComponents(bHome, bFinance, bMembers, bSettings);
     }
 }

53a3ccf09c0d58d8f615a088481dce966c54b6df
cabuze <lucascamp@web.de>
2017-05-17 22:08:42 +0200

    najumodel auskommentiert

--- src/main/java/de/naju/ahlen/controller/ModelController.java
+++ src/main/java/de/naju/ahlen/controller/ModelController.java
@@ -1,13 +1,12 @@
 package de.naju.ahlen.controller;
 
-import de.naju.ahlen.model.NajuModel;
 
 /**
  * Created by Steffen on 27.04.2017.
  */
 public class ModelController {
 
-    private NajuModel najuModel;
+    //private NajuModel najuModel;
 
     public ModelController(){
 
